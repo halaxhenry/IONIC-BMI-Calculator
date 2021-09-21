c58b1866cc389ec1ef741666101fa4f16c687ec4
SeungchulHa <alaxhenry@gmail.com>
2021-09-21 13:32:14 +0900

    how to start ionic

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # IONIC-BMI-Calculator
+
+## 1. install node js
+
+## 2. npm install -g @ionic/cli
+
+## 3. ionic start --> choose framework you want to use
+
+## 4. select template you want
+
+## 5. cd root directory
+
+## 6. ionic serve
+
+## if you want to open it on your on device or simulator
+
+## ionic capacitor add ios
+
+## ionic capacitor open ios
+
+## ionic capacitor copy ios
+
+## ionic capacitor run ios -l --external
---
+++ build/static/js/54.edc857af.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpionic-react"]=this["webpackJsonpionic-react"]||[]).push([[54],{112:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(12),c=e(6),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(n){return Object(c.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.edc857af.chunk.js.map
